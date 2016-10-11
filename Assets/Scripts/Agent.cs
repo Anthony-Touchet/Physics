f6413b48f0509d916b544a50ee6741d035a5c599
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2016-10-11 16:11:29 -0500

    Physics project

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour {
+    public int mass;
+    public Vector3 force;
+
+    // F = mass * velocity
+    // velocity = F / mass
+
+    Vector3 velocity;
+
+    void FixedUpdate () {
+        velocity = force / mass;
+    }
+
+    void LateUpdate()
+    {
+        transform.position += velocity * Time.deltaTime;    //Add velocity to position. Multiply by delta time to make it smooth.
+    }
+}
