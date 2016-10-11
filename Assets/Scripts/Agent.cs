242d131aa67eb8fff88d4c7b7b3ff406c7bde539
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2016-10-11 17:58:37 -0500

    boids.
    Will add more

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -5,16 +5,26 @@ public class Agent : MonoBehaviour {
     public int mass;
     public Vector3 force;
     Vector3 velocity;
-
+    // Applying a Force
     // F = mass * velocity
     // velocity = F / mass
 
+    Vector3 desiredVelocity;
+    public Transform target;
+    Vector3 steering;
+
     void FixedUpdate () {
-        velocity = force / mass;
+        
+        desiredVelocity = (target.position - transform.position).normalized;
+        steering = Vector3.ClampMagnitude(desiredVelocity - velocity, 1).normalized / mass;
+        velocity += steering;
+        if (velocity.magnitude > 5)
+            velocity = velocity.normalized;
     }
 
     void LateUpdate()
     {
-        transform.position += velocity * Time.deltaTime;    //Add velocity to position. Multiply by delta time to make it smooth.
+
+        transform.position += velocity;    //Add velocity to position. Multiply by delta time to make it smooth.
     }
 }
