2d609266654567a6366bf34afdec9ce9d5efbeca
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2016-10-12 08:18:18 -0500

    Camera Follows

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public List<Transform> agents;
+    [Range(0.0f, 1.0f)]public float lookAt;
+
+	// Use this for initialization
+	void Start () {
+        foreach (Agent a in FindObjectsOfType<Agent>())
+        {
+            agents.Add(a.transform);
+        }
+        agents.Add(GameObject.Find("Target").transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 lookAtPoint = Vector3.zero;
+
+        foreach(Transform t in agents)
+        {
+            lookAtPoint += t.position;
+        }
+
+        lookAtPoint = lookAtPoint / agents.Count;
+        lookAtPoint *= lookAt;
+        transform.LookAt(lookAtPoint);
+	}
+}
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,15 +5,16 @@ public class Target : MonoBehaviour {
 
     public GameObject prefab;
     public int agentNumber;
+    public int maxDistance;
 
     // Use this for initialization
-    void Start () {
+    void Awake () {
         for (int i = 0; i < agentNumber; i++)
         {
             Vector3 pos = new Vector3();
-            pos.x = Random.Range(-10, 10);
-            pos.y = Random.Range(-10, 10);
-            pos.z = Random.Range(-10, 10);
+            pos.x = Random.Range(-maxDistance, maxDistance);
+            pos.y = Random.Range(-maxDistance, maxDistance);
+            pos.z = Random.Range(-maxDistance, maxDistance);
 
             GameObject temp = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
             Agent a = temp.GetComponent<Agent>();
